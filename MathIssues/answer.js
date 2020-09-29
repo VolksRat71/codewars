fddd2acfcbb90683d4c49cde2625ae100c4cfb2a
Nathaniel Ryan <nathanieljryan1994@gmail.com>
2020-09-29 16:12:31 -0700

    formatting! Jeez, what a sloppy coder

--- MathIssues/answer.js
+++ MathIssues/answer.js
@@ -4,28 +4,30 @@ Math.round = function (number) {
 
     if (JSON.stringify(number).split(".").length > 1) {
         firstDecimal = JSON.stringify(number).split(".")[1].split("")[0];
-    }
+    };
 
     if (number === 0 || JSON.stringify(number).split(".").length === 1) {
         return parseInt(wholeNumber);
-    }
+    };
 
     if (firstDecimal < 5) {
         return parseInt(wholeNumber)
-    }
+    };
 
     return parseInt(wholeNumber) + 1
 };
 
 Math.ceil = function(number) {
     var wholeNumber = JSON.stringify(number).split(".")[0];
+
     if(JSON.stringify(number).split(".").length > 1){
     return parseInt(wholeNumber) + 1
-    }
-    return parseInt(wholeNumber)
+    };
+
+    return parseInt(wholeNumber);
 };
 
 Math.floor = function(number) {
     var wholeNumber = JSON.stringify(number).split(".")[0];
-    return parseInt(wholeNumber) 
+    return parseInt(wholeNumber) ;
 };
