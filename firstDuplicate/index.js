ccc9a59f2ce8296fbcf45b6254b5f7f9f18e614c
VolksRat71 <nathanieljryan1994@gmail.com>
2020-12-11 18:46:51 -0600

    adding second solution to firstDuplicate

--- firstDuplicate/index.js
+++ firstDuplicate/index.js
@@ -1,12 +1,25 @@
+// Worked in testing, but was to slow on submitting
 function firstDuplicate(a) {
     // Hash set
     var memory = new Array;
     
-    for( let i = 0; i < a.length; i++){
+    for( let i = 0; i < a.length; i++ ){
         if(memory.includes(a[i])){
             return a[i]
         } else {
             memory.push(a[i])
+        }
+    }
+    return -1;
+}
+
+// This one is fast enough 
+function firstDuplicate(a) {
+    for( let i = 0; i < a.length; i++ ){
+        if(a[Math.abs(a[i]) - 1] < 0){
+            return Math.abs(a[i])
+        } else {
+            a[Math.abs(a[i]) - 1] = -a[Math.abs(a[i]) - 1]
         }
     }
     return -1;
